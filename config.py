df399b432624ca0ec1d8a64e86981146069a8a51
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2023-02-17 00:28:42 +0300

    Конфигурация для приложения (Application Configuration)

---
+++ config.py
@@ -0,0 +1,5 @@
+MY_STYLESHEET_OFF = 'QSpinBox::up-button { subcontrol-origin: none; subcontrol-position: center right; width: 0px;}' \
+                    'QSpinBox::down-button { subcontrol-origin: none; subcontrol-position: center left; width: 0px; }'
+
+MY_STYLESHEET_ON = 'QSpinBox::up-button { subcontrol-origin: border; subcontrol-position: top right; } ' \
+                   'QSpinBox::down-button { subcontrol-origin: border; subcontrol-position: bottom right; }'
